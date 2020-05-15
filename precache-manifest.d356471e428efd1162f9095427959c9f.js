09919b8ecf16e0e97657b826650e31f5477fd4fe
Eky Zulianto Arif <ekysambora@gmail.com>
2020-05-15 07:24:37 +0700

    Updates

---
+++ precache-manifest.d356471e428efd1162f9095427959c9f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "558708fa59fbf4caacaf126711cb46bb",
+    "url": "/fabelio-test/index.html"
+  },
+  {
+    "revision": "6bf6bc521b8cdee83dbe",
+    "url": "/fabelio-test/static/css/main.a33115d9.chunk.css"
+  },
+  {
+    "revision": "da90277a33749dc95739",
+    "url": "/fabelio-test/static/js/2.0f5c5a3f.chunk.js"
+  },
+  {
+    "revision": "ef591f8a71697ba15312c367bd720c98",
+    "url": "/fabelio-test/static/js/2.0f5c5a3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6bf6bc521b8cdee83dbe",
+    "url": "/fabelio-test/static/js/main.eb0d0e4a.chunk.js"
+  },
+  {
+    "revision": "909577d6718e357690e3",
+    "url": "/fabelio-test/static/js/runtime-main.f871d240.js"
+  }
+]);
--- src/utils/helper/index.js
+++
@@ -1,4 +0,0 @@
-export function formatPrice(value) {
-	let val = (value / 1).toFixed(0).replace('.', ',')
-	return val.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')
-}
