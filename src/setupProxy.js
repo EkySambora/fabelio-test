5768fa8584a527bed08fa980b96226828ed04d21
Eky Zulianto Arif <ekysambora@gmail.com>
2020-05-15 07:44:59 +0700

    add proxy setting

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,7 +1,7 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function(app) {
   app.use(
-    '/api',
+    'http://www.mocky.io/v2/5c9105cb330000112b649af8',
     createProxyMiddleware({
       target: 'https://ekysambora.github.io/fabelio-test',
       changeOrigin: true,
