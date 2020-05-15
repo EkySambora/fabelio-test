0943ad774e77b3f08e78e385a5c89b4e2151487e
Eky Zulianto Arif <ekysambora@gmail.com>
2020-05-15 07:20:13 +0700

    Added project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,165 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Axios from 'axios';
+import Card from 'components/card';
+import { 
+  Container, 
+  Box, 
+  Header, 
+  Products, 
+  Loading,
+  Checkboxes
+} from './style';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import SearchInput, {createFilter} from 'react-search-input';
+//https://github.com/enkidevs/react-search-input
 
-export default App;
+const baseUrl = "http://www.mocky.io/v2/5c9105cb330000112b649af8";
+
+ class App extends Component {
+   constructor(props) {
+    super(props);
+      this.state = { 
+        products: [],
+        delivery_time:[],
+        checkBoxes: false,
+        furnitureStyles: [],
+        newArrCheckBox:[],
+        searchTerm: '',
+        type: ''
+      }
+   }
+
+  async componentDidMount(){
+    const res = await Axios.get(baseUrl)
+    const dt = await res.data
+    const furnitureStyles = dt.furniture_styles
+    this.setState({products: dt.products})
+
+    let sortArr = Array.from(furnitureStyles).sort();
+    this.setState({furnitureStyles: sortArr})
+
+    this.mapDataDeliveryTime()
+  }
+
+  mapDataDeliveryTime = () => {
+    let arr = []
+    this.state.products.map((item) => {
+      let changeNum = parseInt(item.delivery_time) 
+      arr.push(changeNum)
+      arr.sort((a, b) => a - b)
+      
+      return arr
+    })
+    // remove same value array
+    let backToArray = Array.from(new Set(arr));
+
+    return this.setState({delivery_time: backToArray})
+  }
+
+  showCheckBoxes = () => {
+    this.setState({checkBoxes: !this.state.checkBoxes})
+  }
+  getValueCheck = (item, e)=>{
+    let newArrCheckBox = this.state.newArrCheckBox;
+    let addValue = item;
+    
+    
+    if(e.target.checked ){
+      newArrCheckBox.push(addValue);
+      // console.log(e.target.value);
+      this.setState({newArrCheckBox:newArrCheckBox});
+      this.setState({searchTerm: e.target.value})
+    } else {
+      newArrCheckBox.pop();
+      // console.log(e.target.value);
+
+      this.setState({newArrCheckBox:newArrCheckBox});
+      this.setState({searchTerm: e.target.value})
+    }
+  }
+
+  renderCheckBox(){
+    if(this.state.checkBoxes){
+      // const filterCheckedFurniture = this.state.furnitureStyles.filter(
+      //   (dt) => {
+      //     return dt.indexOf(1) !== -1; 
+      //   }
+      // );
+      return(
+        <Checkboxes>
+          {this.state.furnitureStyles.map((item,i)=> {
+            return (
+              <label key={i} className="container">{item}
+                <input onChange={(e)=>this.getValueCheck(item,e)} type="checkbox" value={item} />
+                <span className="checkmark"></span>
+              </label>
+            )
+          })}
+        </Checkboxes>
+      )
+    }
+  }
+  searchUpdated = (e) => {
+    const dateset = e.target.dataset.set || null;
+    console.log(dateset);
+    
+    this.setState({searchTerm: e.target.value})
+  }
+
+  render() { 
+
+    const KEYS_TO_FILTERS = ['name', 'description', 'price', 'delivery_time', 'furniture_style'];
+    const filteredProducts = this.state.products.filter(createFilter(this.state.searchTerm, KEYS_TO_FILTERS))
+
+    return ( 
+      <>
+        <Container>
+          <Box>
+            <Header>
+
+              <div className="search">
+                <input type="text" placeholder="Search Furniture" data-set="seacrh" onChange={this.searchUpdated} />
+              </div>
+
+              <div className="filter">
+
+                <div className="selectBox"  >
+                  <input 
+                    onClick={this.showCheckBoxes}
+                    readOnly 
+                    data-set="multiple"
+                    type="text" 
+                    placeholder="Furniture Style" 
+                    value={this.state.newArrCheckBox.length ? this.state.newArrCheckBox : ''} 
+                    />
+                  { this.showCheckBoxes ? this.renderCheckBox() : ''}
+                </div>
+
+                <div className="Delivery">
+                  <select data-set="day" onChange={(e)=>this.searchUpdated(e)}>
+                    <option value="">Delivery Time</option>
+                    { this.state.delivery_time.map((item, i) => {
+                      return(
+                        <option key={i} value={item}>{item} Days</option>
+                      )
+                    })}
+                  </select>
+                </div>
+
+              </div>
+            </Header>     
+            { this.state.products.length === 0 ? (
+              <Loading><h5>Loading ...</h5></Loading>) : (
+                <Products onClick={()=> this.setState({checkBoxes: false})}>
+                  { filteredProducts.map((prod, i) => <Card key={i} products={prod} /> )}
+                </Products>
+              )
+            }
+          </Box>
+        </Container>
+      </>
+    );
+  }
+ }
+  
+ export default App;
---
+++ src/data.js
@@ -0,0 +1,73 @@
+export const dataJson = {   
+    "furniture_styles" : [
+        "Classic",
+        "Midcentury",
+        "Scandinavian",
+        "Modern",
+        "Contemporary"
+    ],
+    "products" : [
+        {
+            "name" : "Sofa L Jobi", 
+            "description": "Selama Anda dapat berkumpul bersama keluarga dan o…y time Anda akan terasa 180 derajat perubahannya.", 
+            "furniture_style": [
+                "Classic",
+                "Midcentury"
+            ], 
+            "delivery_time": "14", 
+            "price": 5000000
+        },
+        { 
+            "name" : "Sofa L Vienna", 
+            "description": "Apapun kegiatan ataupun peran Anda dalam kehidupan…akan segala kesibukan dan hiruk-pikuk keseharian.", 
+            "furniture_style": [
+                "Midcentury",
+                "Contemporary"
+            ],
+            "delivery_time": "2", "price": 7999000
+        },
+        { 
+            "name" : "Sofa L Arsa Wooden Leg", 
+            "description": "Arsa 'L' Sofa dengan kaki kayu adalah gabungan dar… heran bila Anda mudah terlelap di atas sofa ini.", 
+            "furniture_style": [
+                "Scandinavian",
+                "Modern"
+            ], 
+            "delivery_time": "7", "price": 9499000
+        },
+        { 
+            "name" : "Sofa L Helena", 
+            "description": "Bagaimana pun style dekorasi hunian, pemilihan war…ada furnitur, Helena L Sofa tak boleh dilewatkan.", 
+            "furniture_style": [
+                "Modern",
+                "Contemporary"
+            ], 
+            "delivery_time": "2", "price": 7499000
+        },
+        { 
+            "name" : "Forbyta Sofa Bed", 
+            "description": "Menikmati waktu liburan sambil bersantai memang pa…r sebagai penyempurna waktu santai Anda di rumah.", 
+            "furniture_style": [
+                "Midcentury"
+            ], 
+            "delivery_time": "28", "price": 8999000
+        },
+        { 
+            "name" : "Sofa Bed Acronap", 
+            "description": "Menikmati waktu liburan sambil bersantai memang pa…r sebagai penyempurna waktu santai Anda di rumah.", 
+            "furniture_style": [
+                "Classic"
+            ], 
+            "delivery_time": "1", "price": 4999000
+        },
+        { 
+            "name" : "Sofa L Wina", 
+            "description": "Apapun kegiatan ataupun peran Anda dalam kehidupan…akan segala kesibukan dan hiruk-pikuk keseharian.", 
+            "furniture_style": [
+                "Scandinavian"
+            ], 
+            "delivery_time": "12", "price": 8999000
+        }
+    ]
+
+}
